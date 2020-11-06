6ba8af004226a8809ac8d17f3e5732cd57cc44ee
celsoaquino <celso.aquino@ymail.com>
2020-11-06 18:47:47 -0300

    Department entity class

---
+++ src/com/celsoaquino/application/Program.java
@@ -0,0 +1,11 @@
+package com.celsoaquino.application;
+
+import com.celsoaquino.model.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
