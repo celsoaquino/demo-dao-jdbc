95de637779fcc232befad640583c46e097e6032f
celsoaquino <celso.aquino@ymail.com>
2020-11-06 18:55:29 -0300

    Seller entity class

--- src/com/celsoaquino/application/Program.java
+++ src/com/celsoaquino/application/Program.java
@@ -1,11 +1,17 @@
 package com.celsoaquino.application;
 
 import com.celsoaquino.model.entities.Department;
+import com.celsoaquino.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(1, "Celso", "celso@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
